2b234c70d72b370d064b9d2d634f8e42485fd88b
r951236958 <r951236958@gmail.com>
2021-03-18 04:06:29 +0800

    禁用gatsby-plugin-webpack-bundle-analyser-v2

--- gatsby-config.js
+++ gatsby-config.js
@@ -2,7 +2,7 @@ require(`dotenv`).config({
   path: `.env`,
 })
 
-const shouldAnalyseBundle = process.env.ANALYSE_BUNDLE
+// const shouldAnalyseBundle = process.env.ANALYSE_BUNDLE
 
 module.exports = {
   siteMetadata: {
@@ -50,13 +50,13 @@ module.exports = {
     `gatsby-plugin-offline`,
     `gatsby-plugin-gatsby-cloud`,
     `gatsby-plugin-netlify`,
-    shouldAnalyseBundle && {
-      resolve: `gatsby-plugin-webpack-bundle-analyser-v2`,
-      options: {
-        analyzerMode: `static`,
-        reportFilename: `_bundle.html`,
-        openAnalyzer: false,
-      },
-    },
+    // shouldAnalyseBundle && {
+    //   resolve: `gatsby-plugin-webpack-bundle-analyser-v2`,
+    //   options: {
+    //     analyzerMode: `static`,
+    //     reportFilename: `_bundle.html`,
+    //     openAnalyzer: false,
+    //   },
+    // },
   ].filter(Boolean),
 }
